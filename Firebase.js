8b6ba3a050c54428bd59cd01b34b9d065dc1a2de
pyroteddie <liammatulick@gmail.com>
2022-04-09 03:55:20 +1000

    1 file modified, 2 added

---
+++ Firebase.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6dKOg2UhZ9ZtGfR69fu0NQbXoMhI7xMI",
+  authDomain: "uflvape.firebaseapp.com",
+  databaseURL: "https://uflvape-default-rtdb.firebaseio.com",
+  projectId: "uflvape",
+  storageBucket: "uflvape.appspot.com",
+  messagingSenderId: "106139754407",
+  appId: "1:106139754407:web:83bc6f215d35d898b4a082",
+  measurementId: "G-T1SKXL2QX2"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var starCountRef = firebase.database().ref('Products/asduhfsdf/');
+starCountRef.on('value', (snapshot) => {
+  const data = snapshot.val();
+  console.log(data)
+});
