e3dd590b30a83dbd6221c8cbca9f45194849d5e1
pyroteddie <liammatulick@gmail.com>
2022-04-10 15:02:41 +1000

    1 file modified, 1 added

---
+++ admin.js
@@ -0,0 +1,13 @@
+
+function SignIn(Email, Password){
+  firebase.auth().signInWithEmailAndPassword(Email, Password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+}
