67dd2fef1b88161b2b7fc27172d99faed141bfc5
pyroteddie <liammatulick@gmail.com>
2022-04-10 15:50:01 +1000

    1 file modified

--- admin.js
+++ admin.js
@@ -1,13 +1,12 @@
 
 function SignIn(){
-  var Email = document.getElementById("SignInEma").value
+  var Email = document.getElementById("SignInEmail").value
   var Password = document.getElementById("SignInPassword").value
 
   firebase.auth().signInWithEmailAndPassword(Email, Password)
   .then((userCredential) => {
-    // Signed in
     var user = userCredential.user;
-    // ...
+    console.log(user)
   })
   .catch((error) => {
     var errorCode = error.code;
