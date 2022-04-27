6dc80add6a9be8bda4f4d20709ef173d10509738
pyroteddie <liammatulick@gmail.com>
2022-04-28 00:42:23 +1000

    1 file modified

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ function Menu3Show() {
 }
 function ProductPage(ProductID){
   var ItemId = ProductID;
-  sesstionStorage.setItem('ID',ItemId)
+  
+  sessionStorage.setItem('ID',ItemId)
   window.location.replace("./pages/productPage.html");
 
 }
