1fccaf18baf34c6381906495784c10ace737eeeb
pyroteddie <liammatulick@gmail.com>
2022-05-01 00:27:29 +1000

    2 files modified

--- assets/js/ComingSoon.js
+++ assets/js/ComingSoon.js
@@ -1,16 +1,40 @@
 var Experquery = firebase.database().ref("Experimental").orderByKey();
-Experquery.once("value")
-  .then(function(snapshot) {
+Experquery.on("value", (snapshot) => {
     snapshot.forEach(function(childSnapshot) {
       var key = childSnapshot.key;
       var childData = childSnapshot.val();
-      console.log(childData);
 
-      var itemCat = childData.Name;
-     
-  
-        document.getElementById("VoteProdContainer").innerHTML += "<div id='VoteProduct'>        <image src='' /><p>"+childData.Name+"</p><p>About</p><p>Ingredients</p><div id='TotalVotes'><a id='UpVotes'>Up Votes: 15</a> <a id='DownVotes'>Down Votes: 5</a></div><button onclick='SendVote("Up")'>Yes</button><button onclick='SendVote("Down")'>No</button></div>";
+      document.getElementById("VoteProdContainer").innerHTML = "<div id='VoteProduct'><image class='VoteImg' src="+childData.Image+" /><div id='InfoContainer'><p class='VoteTitle'>"+childData.Name+"</p><p class='VoteABout'>Description:<br>"+childData.About+"</p><p Class='VoteIng'>Ingredients:<br>"+childData.Ingredients+"</p><div id='TotalVotes'><a id='UpVotes'>Up Votes: "+childData.Votes.Up+"</a> <a id='DownVotes'>Down Votes: "+childData.Votes.Down+"</a></div><div id="+key+"> <button onclick='SendVoteU("+ key +")'>Yes</button><button onclick='SendVoteD("+ key +")'>No</button></div></div></div>";
       
       
   });
+});
+
+function SendVoteU(ID){
+  sessionStorage.setItem('VotedFor', JSON.stringify([{'ProductID':ID,'Voted':'Yes'}]))
+  var VotesUpText = firebase.database().ref('Experimental/'+ID+'/Votes');
+  VotesUpText.once('value', (snapshot) => {
+  const data = snapshot.val();
+  VotesUpText.update({ Up: data.Up + 1});
+ 
+});
+}
+function SendVoteD(ID){
+  sessionStorage.setItem('VotedFor', JSON.stringify([{'ProductID':ID,'Voted':'Yes'}]))
+  var VotesDownText = firebase.database().ref('Experimental/'+ID+'/Votes');
+  VotesDownText.once('value', (snapshot) => {
+    const data = snapshot.val();
+    VotesDownText.update({ Down: data.Down + 1});
+    
+  });
+
+}
+window.addEventListener('storage', () => {
+      var VotesSess = JSON.parse(sessionStorage.VotedFor)
+     VotesSess.forEach(HideVote);
+      function HideVote(item){
+        if(item.Voted === "Yes"){
+          document.getElementById(item.ProductID).style.display = 'none'
+        }
+      }
 });
