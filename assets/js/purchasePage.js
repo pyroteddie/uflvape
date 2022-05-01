3f9344fe990e2efe4999af42831b1ba54a13fa33
pyroteddie <liammatulick@gmail.com>
2022-05-01 21:59:30 +1000

    1 file modified

--- assets/js/purchasePage.js
+++ assets/js/purchasePage.js
@@ -1,11 +1,16 @@
 //let cart = [];
-var cart = [];
-function AddToCart(ID){
-   
-  var ProductInfoCart = firebase.database().ref('Products/' + ID);
-  ProductInfoCart.on('value', (snapshot) => {
-    const data = snapshot.val();
+if(sessionStorage.Cart === undefined){
+  sessionStorage.setItem('Cart',JSON.stringify([]))
+}else{
 
+}
+
+function AddToCart(ID){
+  cart = JSON.parse(sessionStorage.Cart ) || [];
+
+  var ProductInfoCart = firebase.database().ref('Products/' + ID);
+  ProductInfoCart.once('value', (snapshot) => {
+    const data = snapshot.val();
     var item = {
       "name": data.Name,
       "description": data.ID,
