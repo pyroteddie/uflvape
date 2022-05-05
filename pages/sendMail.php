75433b9e42d0750fa9d8519fa5f0ff9bb6bf15ac
pyroteddie <liammatulick@gmail.com>
2022-05-06 07:14:08 +1000

    2 files modified

--- pages/sendMail.php
+++ pages/sendMail.php
@@ -1,11 +1,9 @@
 <?php
-// Multiple recipients
-$to = 'liammatulick@gmail.com'; // note the comma
 
-// Subject
+
+
+$to = 'liammatulick@gmail.com'; 
 $subject = 'Test';
-
-// Message
 $message = '
 <html>
 <head>
@@ -28,14 +26,11 @@ $message = '
 </html>
 ';
 
-// To send HTML mail, the Content-type header must be set
 $headers[] = 'MIME-Version: 1.0';
 $headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
-// Additional headers
-$headers[] = 'From: contact@uflvaping.com';
 
-// Mail it
+$headers[] = 'From: contact@uflvaping.com';
 
 mail($to, $subject, $message, implode("\r\n", $headers));
 $success = mail($to, $subject, $message, implode("\r\n", $headers));
@@ -45,7 +40,44 @@ if (!$success) {
 }
 echo $success;
 ?>
+
 <html>
-<head></Head>
-<body><h1>Sending Email!</h1></body>
+
+<head>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>UVJ</title>
+<link rel="stylesheet" type="text/css" href="../styles.css">
+<script src="../script.js"></script>
+  <style>
+    @font-face { font-family: Gagalin; src: url('https://raw.githubusercontent.com/pyroteddie/uflvape/master/assets/fonts/Gagalin.otf?raw=true') format('opentype')}
+    h1{
+      font-family:Gagalin; 
+      justify-content: center;
+      display:flex;
+      font-size:40px;
+    }
+  </style>
+<h1 >UFL Vape</h1>
+  <div class="NavBar">
+    <button class="NavBarButton" onclick="location.href='../index.html'">HOME</button>
+    <button class="NavBarButton" onclick="location.href='../pages/ComingSoon.html'">COMING SOON</button>
+    <button class="NavBarButton" onclick="location.href='../pages/ContactUs.html'">CONTACT US</button>
+    <span class='CartButContainer'><img class='CartBut' src="https://firebasestorage.googleapis.com/v0/b/uflvape.appspot.com/o/Shopping-Basket-Icon.png?alt=media&token=29eeed85-b3cc-48ee-96a8-7edfad230137" onclick="location.href='../pages/Checkout.html'" /><a id="CartLength"></a></span>
+  </div>
+</head>
+
+<body>
+  <section>
+    <div  class="contactform">
+      <?echo $errorMessage
+      echo '<h1>'$_POST['FullName'] '</h1>'
+      ?>
+    </div >
+  </section>
+
+</body>
+
+
 </html>
+
+
