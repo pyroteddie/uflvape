1c9b1d9fd94ef40d141d96b356dd1cb9b6271029
pyroteddie <liammatulick@gmail.com>
2022-07-21 20:21:39 +1000

    1 file modified

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -37,7 +37,8 @@ Menuquery.once("value")
       var childData = childSnapshot.val();
       
       var itemCat = childData.Fav
-      if (itemCat === 'true' ){
+      console.log(itemCat)
+      if (itemCat === true ){
         document.getElementById("FavGallery").innerHTML += "<li onclick='ProductPage("+ key +")' class='Favcontainer'><img src= " + childData.Image + " alt=" + childData.Name +" Class='Favimage'><div Class='Favoverlay'><p>" + childData.Name +"</p><p>$" + childData.Price +"</p></div></li >";
       
       } 
